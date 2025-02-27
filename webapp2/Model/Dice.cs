d928af01b6b65997b959f0ba827b874e54847e0d
oleeriksen <oleeriksen1958@gmail.com>
2025-02-27 12:00:26 +0100

    div

---
+++ webapp2/Model/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace webapp2.Model
+{
+    public class Dice
+    {
+        private int eyes;
+
+        private int size;
+
+        Random r;
+
+        public Dice(int size = 6)
+        {
+            this.size = size;
+            r = new();
+        }
+
+        public virtual void Roll() {
+            eyes = r.Next(size)+1;
+        }
+
+        public int GetEyes() {
+            return eyes;
+        }
+    }
+}
+
